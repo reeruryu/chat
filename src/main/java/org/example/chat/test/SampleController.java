f298bbe520535f2c0101221a4dfffea9a8a1e048
reeruryu <h2ju1004@gmail.com>
2024-01-12 11:49:41 +0900

    chore: add temporary code for RabbitMQ connection (to be removed)

---
+++ src/main/java/org/example/chat/test/SampleController.java
@@ -0,0 +1,22 @@
+package org.example.chat.test;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class SampleController {
+
+    private static final String EXCHANGE_NAME = "sample.exchange";
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @GetMapping("sample/queue")
+    public String samplePublish() {
+        rabbitTemplate.convertAndSend(EXCHANGE_NAME, "sample.routing.#", "RabbitMQ + SpringBoot = Success");
+        return "Message sending!";
+    }
+}
---
+++ src/main/java/org/example/chat/test/SampleListener.java
@@ -0,0 +1,18 @@
+package org.example.chat.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SampleListener {
+
+    private static final Logger log = LoggerFactory.getLogger(SampleListener.class);
+
+    @RabbitListener(queues = "sample.queue")
+    public void receiveMessage(final Message message) {
+        log.info(message.toString());
+    }
+}
