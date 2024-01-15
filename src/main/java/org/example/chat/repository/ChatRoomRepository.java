888c8d0cc8cf9e6fa408e18fc05bb0e7fa6bf291
reeruryu <h2ju1004@gmail.com>
2024-01-15 12:01:50 +0900

    chore: unambiguous imports on fly

--- src/main/java/org/example/chat/repository/ChatRoomRepository.java
+++ src/main/java/org/example/chat/repository/ChatRoomRepository.java
@@ -1,6 +1,5 @@
 package org.example.chat.repository;
 
-import org.example.chat.entity.ChatMessage;
 import org.example.chat.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
