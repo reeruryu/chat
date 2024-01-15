76464e764c22da12afd9b2fe50af38e47aba2be2
reeruryu <h2ju1004@gmail.com>
2024-01-15 11:21:51 +0900

    feat: create chat-related entity and repository

---
+++ src/main/java/org/example/chat/entity/ChatMessage.java
@@ -0,0 +1,34 @@
+package org.example.chat.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "CHAT_MESSAGE")
+public class ChatMessage {
+
+    @Id
+    @Column(name = "chat_message_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id")
+    private ChatRoom chatRoom;
+
+    @Column(name = "content")
+    private String content;
+
+    //private LocalDateTime localDateTime;
+
+}
---
+++ src/main/java/org/example/chat/entity/User.java
@@ -0,0 +1,22 @@
+package org.example.chat.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "USER")
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name", length = 255)
+    private String userName;
+
+}
