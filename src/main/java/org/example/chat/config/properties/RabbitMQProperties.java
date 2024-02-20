89ae75c7abe61e22f5a84b5a4d71af840bc6c51f
reeruryu <h2ju1004@gmail.com>
2024-02-20 16:46:35 +0900

    refactor: extract Rabbitmq properties into class

---
+++ src/main/java/org/example/chat/config/properties/RabbitMQProperties.java
@@ -0,0 +1,27 @@
+package org.example.chat.config.properties;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+public class RabbitMQProperties {
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.stompPort}")
+    private int stompPort;
+
+}
